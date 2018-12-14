3e9996e3905854b36015185022fc3dedaa6d6763
Anthony Sheldrake <anthonysheldrake@Anthonys-MacBook-Pro.local>
2018-12-14 15:30:34 +0000

    switched keeplive to http

--- server/cron/herokuKeepalive.js
+++ server/cron/herokuKeepalive.js
@@ -1,3 +1,3 @@
 var http = require("http");
-http.get("https://coolr-wp-ooobot.herokuapp.com/webhook?keepalive");
+http.get("http://coolr-wp-ooobot.herokuapp.com/webhook?keepalive");
 console.log('Ran Heroku Keep-alive')
